7041d40ecf8b4bfdfa500f13549d2422d8ebabe9
yasuba <mayadrivermd@gmail.com>
2016-10-10 09:28:55 +0100

    trying to use futures and getting it all wrong

--- src/main/scala/Controllers/myController.scala
+++ src/main/scala/Controllers/myController.scala
@@ -7,7 +7,10 @@ import com.twitter.finatra.http.{HttpServer, Controller}
 import com.twitter.finatra.request.{QueryParam, RouteParam}
 import java.nio.file.{ Paths, Files }
 
-import rmq.Producer
+import rmq.{Consumer1, Producer}
+
+import scala.concurrent.ExecutionContext
+import scala.util.{Failure, Success}
 
 
 case class HelloRequest(@RouteParam platform: String, @QueryParam feature: Option[String])
@@ -15,24 +18,34 @@ case class HelloRequest(@RouteParam platform: String, @QueryParam feature: Optio
 case class Foo(name: String)
 
 class MyController @Inject() extends Controller {
+  implicit val ec = ExecutionContext.global
+
   get("/") { request: Request =>
     response.ok.view("homepage.mustache", Foo("Bob"))
   }
 
   post("/pdf-me") { request: Request =>
     val firstName: String = request.params("firstname")
-    val lastName = request.params("lastname")
+    val emailAddress = request.params("emailAddress")
 
     val examples = Paths.get("./example")
     val `test.pdf` = examples resolve "test.pdf"
 
     val pdf: Array[Byte] =
-      (firstName + lastName).getBytes
+      (firstName + emailAddress).getBytes
 
     println("publishing message")
-    Producer.sendMessage(pdf)
-    Files write (`test.pdf`, pdf)
+    val messageSent = Producer.sendMessage(pdf)
+    messageSent.onComplete {
+      case Success(response) => {
+        println("message sent")
+        Consumer1.test
+      }
+      case Failure(error) => println(error)
+    }
 
+//    Files write (`test.pdf`, pdf)
+    response.ok.body("Your email will be sent shortly")
   }
 
   get("/ping") { request: Request =>
--- src/main/scala/rmq/Rmq.scala
+++ src/main/scala/rmq/Rmq.scala
@@ -2,16 +2,22 @@ package rmq
 
 import java.util.concurrent.TimeUnit
 
-import akka.actor.{Actor, Props, ActorSystem}
+import akka.actor.{ActorRef, Actor, Props, ActorSystem}
+import akka.pattern.ask
+import akka.util.Timeout
 import com.github.sstone.amqp.Amqp._
-import com.github.sstone.amqp.{Amqp, Consumer, ChannelOwner, ConnectionOwner}
+import com.github.sstone.amqp.RpcClient.{Response, Request}
+import com.github.sstone.amqp.RpcServer.{ProcessResult, IProcessor}
+import com.github.sstone.amqp._
 import com.rabbitmq.client.ConnectionFactory
+import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration._
-import scala.util.Random
+import scala.util.{Failure, Success, Random}
 
 
 object Producer {
   implicit val system = ActorSystem("mySystem")
+  implicit val ec = ExecutionContext.global
 
   // create an AMQP connection
   val connFactory = new ConnectionFactory()
@@ -23,34 +29,53 @@ object Producer {
   waitForConnection(system, conn, producer).await(5, TimeUnit.SECONDS)
 
   def sendMessage(message: Array[Byte]) =
-    producer ! Publish("amq.direct", "my_key", message, properties = None, mandatory = true, immediate = false)
+    Future(producer ! Publish("amq.direct", "my_key", message, properties = None, mandatory = true, immediate = false))
 
   // give it some time before shutting everything down
   Thread.sleep(500)
   system.shutdown()
 }
 
-object Consumer1 extends App {
+object Consumer1 {
+
+  def test = println("yo")
   implicit val system = ActorSystem("mySystem")
+  implicit val ec = ExecutionContext.global
 
   // create an AMQP connection
   val connFactory = new ConnectionFactory()
   connFactory.setUri("amqp://guest:guest@localhost/%2F")
   val conn = system.actorOf(ConnectionOwner.props(connFactory, 1 second))
+  val queueParams = QueueParameters("my_queue", passive = false, durable = false, exclusive = false, autodelete = true)
 
-  // create an actor that will receive AMQP deliveries
-  val listener = system.actorOf(Props(new Actor {
-    def receive = {
-      case Delivery(consumerTag, envelope, properties, body) => {
-        println("got a message: " + new String(body))
-        sender ! Ack(envelope.getDeliveryTag)
-      }
+//   create an actor that will receive AMQP deliveries
+//  val listener: ActorRef = system.actorOf(Props(new Actor {
+//    def receive = {
+//      case Delivery(consumerTag, envelope, properties, body) => {
+//        println("got a message: " + new String(body))
+//        Future(body)
+//      }
+//    }
+//  }))
+
+//  val consumer = ConnectionOwner.createChildActor(conn, Consumer.props(listener, channelParams = None, autoack = false))
+
+//
+  val processor = new IProcessor {
+    def process(delivery: Delivery) = {
+      // assume that the message body is a string
+      val response = "response to " + new String(delivery.body)
+      Future(ProcessResult(Some(response.getBytes)))
     }
-  }))
+    def onFailure(delivery: Delivery, e: Throwable) = ProcessResult(None) // we don't return anything
+  }
+
+  val consumer: ActorRef = ConnectionOwner.createChildActor(conn, RpcServer.props(queueParams, StandardExchanges.amqDirect,  "my_key", processor, ChannelParameters(qos = 1)))
+
 
   // create a consumer that will route incoming AMQP messages to our listener
   // it starts with an empty list of queues to consume from
-  val consumer = ConnectionOwner.createChildActor(conn, Consumer.props(listener, channelParams = None, autoack = false))
+//  val consumer = ConnectionOwner.createChildActor(conn, Consumer.props(listener, channelParams = None, autoack = false))
 
   // wait till everyone is actually connected to the broker
   Amqp.waitForConnection(system, consumer).await()
@@ -60,12 +85,30 @@ object Consumer1 extends App {
   // the broker is lost: queue and binding will be gone
 
   // create a queue
-  val queueParams = QueueParameters("my_queue", passive = false, durable = false, exclusive = false, autodelete = true)
   consumer ! DeclareQueue(queueParams)
   // bind it
   consumer ! QueueBind(queue = "my_queue", exchange = "amq.direct", routing_key = "my_key")
   // tell our consumer to consume from it
   consumer ! AddQueue(QueueParameters(name = "my_queue", passive = false))
+
+  val rpcClient: ActorRef = ConnectionOwner.createChildActor(conn, RpcClient.props())
+//
+  implicit val timeout: Timeout = 2 seconds
+//
+  val receive = for (i <- 0 to 5) {
+    println("receiving...")
+    val request: Array[Byte] = ("request " + i).getBytes
+//
+    val f: Future[Response] = (consumer ? Request(List(Publish("amq.direct", "my_key", request)), 3)).mapTo[RpcClient.Response]
+    f.onComplete {
+      case Success(response) => {
+        response.deliveries.foreach(delivery => println("message received: " + new String(delivery.body)))
+      }
+      case Failure(error) => println(error)
+    }
+  }
+
+
 
   // run the Producer sample now and see what happens
   println("Waiting for messages. Press enter to exit...")
